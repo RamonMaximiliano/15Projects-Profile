5688ed858aa708e185b370a32fe9e068f9fc3d2e
Ramon <ramon_max1986@hotmail.com>
2022-02-09 17:54:07 -0300

    started

---
+++ script.js
@@ -0,0 +1,22 @@
+/* Top:
+10 - Simple login form
+Form validator
+Use login form to provide data to the profile card
+
+Middle:
+8 - Profile card
+
+In the bottom:
+5 - Social media icons
+Rate the site
+6 - star rating
+
+----------------------------------------------------------------------------------------------------
+OTHER PROJECT:
+
+11 - Vertical navigation menu
+12 - get data with fetch
+7 - Stopwatch
+14 - Gloogle clone
+
+---------------------------------------------------------------------------------------------------- */
