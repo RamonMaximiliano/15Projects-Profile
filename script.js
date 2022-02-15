8441987519e85564770850b29bd17144d067a494
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-02-14 23:22:00 -0300

    form validation complete

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 /* Top:
-10 - Simple login form
-Form validator
-Use login form to provide data to the profile card, create text
-
-Middle:
-8 - Profile card
-
-Create upload image button
-
-
 Rate the site
 6 - star rating
-
-
 
 ----------------------------------------------------------------------------------------------------
 OTHER PROJECT:
@@ -23,3 +11,49 @@ OTHER PROJECT:
 14 - Google clone
 
 ---------------------------------------------------------------------------------------------------- */
+
+function createProfile() {
+    let name = document.getElementById("username").value
+
+    //VALIDATING USERNAME
+    if (name != '') {
+
+        let password = document.getElementById("password").value
+        let firstPart = password.split("@")
+
+        //validating the @ in the e-mail, and the quantity of @s
+        if (firstPart.length < 2 || firstPart.length > 2) {
+            window.alert("You did not type a valid e-mail!")
+
+        } else {
+
+            //VALIDATING THE .com in the e-mail and the quantityt of .coms
+            let array2 = firstPart[1]
+            let secondPart = array2.split(".com")
+
+            console.log(firstPart[1])
+            console.log(secondPart)
+
+            if (secondPart[1] != '' || secondPart.length > 2) {
+                window.alert("You did not type a valid e-mail!")
+            } else {
+                let imageLoaded = document.getElementById("Image").files[0];
+
+                //validating if an image was provided
+                if (imageLoaded == undefined) {
+                    window.alert("You did not provide an image!")
+                } else {
+
+                    //é obrigatório o .src= URL.createObjectURL(imageLoaded)
+                    document.getElementById("uploadedImage").src = URL.createObjectURL(imageLoaded)
+                    document.querySelector("h1.profile-title").innerHTML = name
+                    document.querySelector("p.profile-description").innerHTML = `My username is <strong>${name}</strong> and my fake password is <strong>${password}</strong>. <br><br>This is an exercise simple page for the <strong>15 Projects</strong> on a daily basis.`
+
+                }
+            }
+        }
+    } else {
+        window.alert("You did not type the Username!")
+    }
+
+}
