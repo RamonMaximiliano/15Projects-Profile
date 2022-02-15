9db0fa6f71eae88902018c7e445b945506a5d6b3
Ramon <ramon_max1986@hotmail.com>
2022-02-15 17:56:53 -0300

    Finished

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-/* Top:
-Rate the site
-6 - star rating
-
-//Add eventlistener and according to the ball clicked rank the site
-
-----------------------------------------------------------------------------------------------------
-OTHER PROJECT:
-
-11 - Vertical navigation menu
-12 - get data with fetch
-7 - Stopwatch
-14 - Google clone
-
----------------------------------------------------------------------------------------------------- */
-
 function createProfile() {
     let name = document.getElementById("username").value
 
@@ -49,7 +33,7 @@ function createProfile() {
                     //é obrigatório o .src= URL.createObjectURL(imageLoaded)
                     document.getElementById("uploadedImage").src = URL.createObjectURL(imageLoaded)
                     document.querySelector("h1.profile-title").innerHTML = name
-                    document.querySelector("p.profile-description").innerHTML = `My username is <strong>${name}</strong> and my fake password is <strong>${password}</strong>. <br><br>This is an exercise simple page for the <strong>15 Projects</strong> on a daily basis.`
+                    document.querySelector("p.profile-description").innerHTML = `My username is <strong>${name}</strong> and my fake E-mail is <strong>${password}</strong>. <br><br>This is an exercise simple page for the <strong>15 Projects</strong> on a daily basis.`
 
                 }
             }
@@ -60,3 +44,54 @@ function createProfile() {
 }
 
 
+let ratingArray = document.querySelectorAll("div.rating");
+
+//Getting ID of elenet clicked from array of elements
+for (let ball of ratingArray) {
+    ball.addEventListener("click", ratePage)
+}
+
+function ratePage(events) {
+    let rateResult = events.target.id
+
+    if (rateResult == 'star1') {
+        document.getElementById("star1").classList.add("star1clicked")
+        document.getElementById("star2").classList.add("star1clicked")
+        document.getElementById("star3").classList.add("star1clicked")
+        document.getElementById("star4").classList.add("star1clicked")
+        document.getElementById("star5").classList.add("star1clicked")
+        document.querySelector("div.finalresult").innerHTML = ("<strong><h1 style='color:red;'>BAD</h1></strong>")
+    } else if (rateResult == 'star2') {
+        document.getElementById("star1").classList.add("star2clicked")
+        document.getElementById("star2").classList.add("star2clicked")
+        document.getElementById("star3").classList.add("star2clicked")
+        document.getElementById("star4").classList.add("star2clicked")
+        document.getElementById("star5").classList.add("star2clicked")
+        document.querySelector("div.finalresult").innerHTML = ("<strong><h1 style='color:orange;'>Ok</h1></strong>")
+    }
+    else if (rateResult == 'star3') {
+        document.getElementById("star1").classList.add("star3clicked")
+        document.getElementById("star2").classList.add("star3clicked")
+        document.getElementById("star3").classList.add("star3clicked")
+        document.getElementById("star4").classList.add("star3clicked")
+        document.getElementById("star5").classList.add("star3clicked")
+        document.querySelector("div.finalresult").innerHTML = ("<strong><h1 style='color:rgb(129, 127, 3);'>Fine</h1></strong>")
+    }
+    else if (rateResult == 'star4') {
+        document.getElementById("star1").classList.add("star4clicked")
+        document.getElementById("star2").classList.add("star4clicked")
+        document.getElementById("star3").classList.add("star4clicked")
+        document.getElementById("star4").classList.add("star4clicked")
+        document.getElementById("star5").classList.add("star4clicked")
+        document.querySelector("div.finalresult").innerHTML = ("<strong><h1 style='color:rgb(164, 192, 6);'>Good</h1></strong>")
+    }
+    else if (rateResult == 'star5') {
+        document.getElementById("star1").classList.add("star5clicked")
+        document.getElementById("star2").classList.add("star5clicked")
+        document.getElementById("star3").classList.add("star5clicked")
+        document.getElementById("star4").classList.add("star5clicked")
+        document.getElementById("star5").classList.add("star5clicked")
+        document.querySelector("div.finalresult").innerHTML = ("<strong><h1 style='color:green;'>Excellent</h1></strong>")
+    }
+
+}
